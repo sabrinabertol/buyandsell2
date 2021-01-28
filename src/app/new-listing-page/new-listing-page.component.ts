787f45c3ab00d6c36e73eea409b782cbdecaa3f8
Sabrina Bertol <bertol.sabrina@gmail.com>
2021-01-28 23:39:27 +0000

    first late commit

---
+++ src/app/new-listing-page/new-listing-page.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-new-listing-page',
+  templateUrl: './new-listing-page.component.html',
+  styleUrls: ['./new-listing-page.component.scss']
+})
+export class NewListingPageComponent implements OnInit {
+  name: string = '';
+  description: string = '';
+  price: string = '';
+
+  constructor(
+   private router: Router,
+  ) { }
+
+  ngOnInit(): void {
+    alert('Creating a new listing...');
+    this.router.navigateByUrl('/my-listings');
+  }
+
+}
